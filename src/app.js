9be06fa692f93964c282d655773f9685c8fd2abe
Your Name <you@example.com>
2026-02-01 12:20:18 +0300

    hw4

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+const { PORT = 3000, API_URL = "http://127.0.0.1" } = process.env;
+
+app.get("/", (request, response) => {
+  response.status(200);
+  response.send("Hello, World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен по адресу ${API_URL}:${PORT}`);
+});
